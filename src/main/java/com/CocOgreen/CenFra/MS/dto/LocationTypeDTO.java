efff35bdcd6590234696fef97139e1040fa0d9de
AnhNDSE8125 <ducanh.nguyenkl@gmail.com>
2026-01-24 21:23:03 +0700

    chore: Init DTO, Mapper

--- src/main/java/com/CocOgreen/CenFra/MS/dto/LocationTypeDTO.java
+++ src/main/java/com/CocOgreen/CenFra/MS/dto/LocationTypeDTO.java
@@ -1,4 +1,13 @@
 package com.CocOgreen.CenFra.MS.dto;
 
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class LocationTypeDTO {
+
+    private Integer locationTypeId;
+    private String code;
 }
