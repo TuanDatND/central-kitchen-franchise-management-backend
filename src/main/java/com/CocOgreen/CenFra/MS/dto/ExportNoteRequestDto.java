37de87f8a68f8996f87790877e81c0e634b63b19
Nguyen Dinh Tuan Dat <legendboy.vn@gmail.com>
2026-02-09 19:41:24 +0700

    fix :fix class exportNoteRequireDto

--- src/main/java/com/CocOgreen/CenFra/MS/dto/ExportNoteRequestDto.java
+++ src/main/java/com/CocOgreen/CenFra/MS/dto/ExportNoteRequestDto.java
@@ -1,11 +1,10 @@
 package com.CocOgreen.CenFra.MS.dto;
 
 import lombok.Data;
-import org.antlr.v4.runtime.misc.NotNull;
+
 
 @Data
 public class ExportNoteRequestDto {
-    @NotNull(message = "ID đơn hàng không được để trống")
     private Integer storeOrderId;
 
     private String note;
