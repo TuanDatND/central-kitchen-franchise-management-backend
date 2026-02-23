59444af301b0ce88b460c568c936179db4b601cd
Nguyen Duc Anh <ducanh.nguyenkl@gmail.com>
2026-02-23 21:04:50 +0700

    Update RoleMapper.java

--- src/main/java/com/CocOgreen/CenFra/MS/mapper/RoleMapper.java
+++ src/main/java/com/CocOgreen/CenFra/MS/mapper/RoleMapper.java
@@ -3,7 +3,6 @@ package com.CocOgreen.CenFra.MS.mapper;
 import com.CocOgreen.CenFra.MS.dto.RoleDTO;
 import com.CocOgreen.CenFra.MS.entity.Role;
 import org.mapstruct.Mapper;
-<<<<<<< feature/Login-Authorize
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
