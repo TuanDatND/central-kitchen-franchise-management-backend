870babfe4359f636eaf726d57ba83a5684ea276c
Nguyen Duc Anh <ducanh.nguyenkl@gmail.com>
2026-02-23 20:57:53 +0700

    Update OrderDetailMapper.java

--- src/main/java/com/CocOgreen/CenFra/MS/mapper/OrderDetailMapper.java
+++ src/main/java/com/CocOgreen/CenFra/MS/mapper/OrderDetailMapper.java
@@ -8,17 +8,11 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface OrderDetailMapper {
 
-<<<<<<< feature/Login-Authorize
     @Mapping(source = "product.productId", target = "productId")
     OrderDetailDTO toDto(OrderDetail entity);
 
     @Mapping(target = "storeOrder", ignore = true)
     @Mapping(target = "product", ignore = true)
     OrderDetail toEntity(OrderDetailDTO dto);
-=======
-    @Mapping(target = "productId", source = "product.productId")
-    @Mapping(target = "productName", source = "product.productName")
-    @Mapping(target = "unit", source = "product.unit")
-    OrderDetailDTO toDTO(OrderDetail orderDetail);
->>>>>>> main
+
 }
