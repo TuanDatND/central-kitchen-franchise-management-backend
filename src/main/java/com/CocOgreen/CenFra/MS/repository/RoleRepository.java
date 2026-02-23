e567df19eb8fb56ae0d5c8fbdbdbbfd06a0a44d8
Nguyen Duc Anh <ducanh.nguyenkl@gmail.com>
2026-02-23 20:56:03 +0700

    Update RoleRepository.java

--- src/main/java/com/CocOgreen/CenFra/MS/repository/RoleRepository.java
+++ src/main/java/com/CocOgreen/CenFra/MS/repository/RoleRepository.java
@@ -1,10 +1,7 @@
 package com.CocOgreen.CenFra.MS.repository;
 
 import com.CocOgreen.CenFra.MS.entity.Role;
-<<<<<<< feature/Login-Authorize
 import com.CocOgreen.CenFra.MS.enums.RoleName;
-=======
->>>>>>> main
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
