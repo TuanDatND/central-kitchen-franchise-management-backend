da3783dee955e93411fea6c5780de4a9c8038b91
Quang9325 <quangtran442005@gmail.com>
2026-03-07 11:41:43 +0700

    feat: create inventory receipt and get product batchs

--- src/main/java/com/CocOgreen/CenFra/MS/repository/ProductBatchRepository.java
+++ src/main/java/com/CocOgreen/CenFra/MS/repository/ProductBatchRepository.java
@@ -14,12 +14,16 @@ public interface ProductBatchRepository extends JpaRepository<ProductBatch, Inte
     // Cần thiết để tìm lô hàng khi quét mã nhập kho
     Optional<ProductBatch> findByBatchCode(String batchCode);
 
+    // Lấy danh sách lô hàng theo trạng thái (Ví dụ: WAITING_FOR_STOCK)
+    List<ProductBatch> findByStatus(com.CocOgreen.CenFra.MS.enums.BatchStatus status);
+
     // Kiểm tra mã lô trùng lặp khi tạo mới
     boolean existsByBatchCode(String batchCode);
 
     // Tìm tất cả các lô hàng được tạo ra từ một Lệnh sản xuất cụ thể
     List<ProductBatch> findByManufacturingOrder_ManuOrderId(Integer manuOrderId);
 
-    //TuanDatCutee hehehe
-    List<ProductBatch> findByProductAndCurrentQuantityGreaterThanOrderByExpiryDateAsc(Product product, Integer quantity);
+    // TuanDatCutee hehehe
+    List<ProductBatch> findByProductAndCurrentQuantityGreaterThanOrderByExpiryDateAsc(Product product,
+            Integer quantity);
 }
