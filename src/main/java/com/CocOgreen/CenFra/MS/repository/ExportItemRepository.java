ba234e1d25da4554a3c170a10451f056c2d7d4e9
Nguyen Dinh Tuan Dat <legendboy.vn@gmail.com>
2026-03-09 19:29:10 +0700

    feat: Add ExportItem repository, service, and controller for listing items.

--- src/main/java/com/CocOgreen/CenFra/MS/repository/ExportItemRepository.java
+++ src/main/java/com/CocOgreen/CenFra/MS/repository/ExportItemRepository.java
@@ -1,7 +1,11 @@
 package com.CocOgreen.CenFra.MS.repository;
 
 import com.CocOgreen.CenFra.MS.entity.ExportItem;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExportItemRepository extends JpaRepository<ExportItem, Integer> {
+    Page<ExportItem> findAll(Pageable pageable);
+
 }
--- src/main/java/com/CocOgreen/CenFra/MS/service/ExportItemService.java
+++ src/main/java/com/CocOgreen/CenFra/MS/service/ExportItemService.java
@@ -1,4 +1,22 @@
 package com.CocOgreen.CenFra.MS.service;
 
+import com.CocOgreen.CenFra.MS.dto.ExportItemDto;
+import com.CocOgreen.CenFra.MS.entity.ExportItem;
+import com.CocOgreen.CenFra.MS.mapper.ExportItemMapper;
+import com.CocOgreen.CenFra.MS.repository.ExportItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ExportItemService {
+    private final ExportItemRepository exportItemRepository;
+    private  final ExportItemMapper exportItemMapper;
+
+    public Page<ExportItemDto> findAll(Pageable pageable) {
+        Page<ExportItem> exportItems = exportItemRepository.findAll(pageable);
+        return exportItems.map(exportItemMapper::toDto);
+    }
 }
