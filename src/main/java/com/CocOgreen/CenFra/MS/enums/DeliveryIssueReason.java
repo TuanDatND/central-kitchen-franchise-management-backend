d58e662cf62c757d29e66fe637ec0288ccb76b3e
AnhNDSE8125 <ducanh.nguyenkl@gmail.com>
2026-03-23 19:23:32 +0700

    feat(issue): expand delivery issue workflow

---
+++ src/main/java/com/CocOgreen/CenFra/MS/enums/DeliveryIssueReason.java
@@ -0,0 +1,10 @@
+package com.CocOgreen.CenFra.MS.enums;
+
+public enum DeliveryIssueReason {
+    DAMAGED,
+    MISSING_ITEMS,
+    WRONG_ITEMS,
+    QUALITY_FAILED,
+    LATE_DELIVERY,
+    REFUSED_DELIVERY
+}
